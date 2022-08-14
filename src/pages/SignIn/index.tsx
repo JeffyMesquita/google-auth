175db63666f7a721f1c94a8713bcc2069a2bf616
Jeferson Mesquita <je_2742@hotmail.com>
2022-08-13 23:34:10 -0300

    first commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { GoogleLogo } from 'phosphor-react';
+
+import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
+import { auth } from '../../services/firebase';
+
+import './styles.scss';
+
+export function SignIn() {
+  const [user, setUser] = useState<User>({} as User);
+
+  function handleGoogleSignIn() {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
+      }).catch(error => {
+        console.log(error);
+      }
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="user">
+        {user.photoURL && <img src={user.photoURL} alt="Foto do usuário" />}
+        <strong>{user.displayName}</strong>
+        <small>{user.email}</small>
+      </div>
+      
+      
+
+      <h1>Acesse sua conta</h1>
+
+      <span>
+        Utilizando autenticação social, por exemplo, autenticação do Google você{' '}
+        <br />
+        facilita a vida do usuário permitindo utilizar sua aplicação sem fazer
+        cadastro
+      </span>
+
+      <button className="button" type="button" onClick={handleGoogleSignIn}>
+        <GoogleLogo />
+        Entrar com Google
+      </button>
+    </div>
+  );
+}
